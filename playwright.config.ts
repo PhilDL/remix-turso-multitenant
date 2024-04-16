631ec760b55b364c54eae7a3005fac7109af9385
Philippe L'ATTENTION <philippe.lattention@hotmail.fr>
2024-04-16 22:06:41 +0400

    test

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,11 @@
-import 'dotenv/config'
-import { defineConfig, devices } from '@playwright/test'
+import "dotenv/config";
 
-const PORT = process.env.PORT || '3000'
+import { defineConfig, devices } from "@playwright/test";
+
+const PORT = process.env.PORT || "3000";
 
 export default defineConfig({
-  testDir: './tests/e2e',
+  testDir: "./tests/e2e",
   timeout: 15 * 1000,
   expect: {
     timeout: 5 * 1000,
@@ -13,29 +14,29 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: "html",
   use: {
     baseURL: `http://localhost:${PORT}/`,
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   projects: [
     {
-      name: 'chromium',
+      name: "chromium",
       use: {
-        ...devices['Desktop Chrome'],
+        ...devices["Desktop Chrome"],
       },
     },
   ],
 
   webServer: {
-    command: process.env.CI ? 'npm run start:mocks' : 'npm run dev',
+    command: process.env.CI ? "pnpm run start:mocks" : "pnpm run dev",
     port: Number(PORT),
     reuseExistingServer: !process.env.CI,
-    stdout: 'pipe',
-    stderr: 'pipe',
+    stdout: "pipe",
+    stderr: "pipe",
     env: {
       PORT,
     },
   },
-})
+});
