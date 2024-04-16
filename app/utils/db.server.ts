54adaa56b6a82ce2b61543b17508558356315d82
Philippe L'ATTENTION <philippe.lattention@hotmail.fr>
2024-04-16 23:20:58 +0400

    no embedded

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -1,4 +1,4 @@
-import { createClient } from "@libsql/client";
+import { createClient } from "@libsql/client/http";
 import { drizzle } from "drizzle-orm/libsql";
 
 import { env } from "~/env.server";
@@ -7,12 +7,13 @@ import * as schema from "../../drizzle/schema";
 export function buildDbClient() {
   const url = env.TURSO_DB_URL.trim();
   const authToken = env.TURSO_DB_AUTH_TOKEN.trim();
-  const local = env.TURSO_LOCAL_DB.trim();
+  // const local = env.TURSO_LOCAL_DB.trim();
 
   return drizzle(
     createClient({
-      url: local,
-      syncUrl: url,
+      // url: local,
+      // syncUrl: url,
+      url,
       authToken,
       encryptionKey: env.SESSION_SECRET,
     }),
