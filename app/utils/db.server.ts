28cf1b859927f5a6e16bf644265ebb128df309bf
Philippe L'ATTENTION <philippe.lattention@hotmail.fr>
2024-04-16 23:13:25 +0400

    pfffff

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -1,4 +1,4 @@
-import { createClient } from "@libsql/client";
+import { createClient } from "@libsql/client/http";
 import { drizzle } from "drizzle-orm/libsql";
 
 import { env } from "~/env.server";
